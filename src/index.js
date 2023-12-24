8ac9a6eda982ae1c2c1542817be0040646321b60
AbdurRahman-Khalil <m.abdurrahmankhalil2@gmail.com>
2023-12-24 16:51:53 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import reportWebVitals from './reportWebVitals';
-import QRCodeGenerator from './QRCodeGenerator';
+
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-    <QRCodeGenerator />
-    
-    {/* <RandomPic /> */}
-    
+    <App />
+
+
+
+
+
+
+
+
+
+
+
+    {/* <GamingHeader />
+    <GamingGamesSection />
+    <GamingMenu /> */}
+
     {/* <Card
       image='https://randomuser.me/api/portraits/med/men/31.jpg'
       name='Ali'
@@ -19,9 +33,9 @@ root.render(
       description='He is a hard working developer'
       year_joined='2010'
       friends={230}
-    />
+    /> */}
 
-    <Card
+    {/* <Card
       image='https://randomuser.me/api/portraits/med/men/32.jpg'
       name='Haseeb'
       category='Senior Developer'
@@ -30,12 +44,10 @@ root.render(
       friends={290}
     /> */}
 
-    {/* <Counter /> */}
-      
   </React.StrictMode>
 );
 
+reportWebVitals();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
