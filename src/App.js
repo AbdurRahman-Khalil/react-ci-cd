4b86ad6eb2dfb4bc615d2d2b82ebb3a89a8785e4
AbdurRahman-Khalil <m.abdurrahmankhalil2@gmail.com>
2023-12-24 19:57:03 +0500

    more changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello from React.js</h1>
+      <h1>Hello from React.js CI CD</h1>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
